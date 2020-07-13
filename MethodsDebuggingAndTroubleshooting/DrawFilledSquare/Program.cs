b3f12b5541cff4a5bc0b109fb917ddcc96b44c66
Mariyana Boyadzhieva <boyadzhieva99@gmail.com>
2020-07-14 00:09:39 +0300

    PrintingTriangle and DrawFilledSquare added

---
+++ MethodsDebuggingAndTroubleshooting/DrawFilledSquare/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace DrawFilledSquare
+{
+    using System;
+
+    // From SoftUni Programming Fundamentals January 2018 - Lab: Methods, Debugging and Troubleshooting Code
+    public class Program
+    {
+
+        /*
+         Printing a square simillar to this one, the size of which is taken from the input
+         --------
+         -\/\/\/-
+         -\/\/\/-
+         --------
+        (here the size is 4)
+        */
+
+        public static void Main()
+        {
+            Console.Write("Size of the square: ");
+            int n = int.Parse(Console.ReadLine());
+
+            PrintDashes(n);
+            PrintMiddle(n);
+            PrintDashes(n);
+        }
+
+        private static void PrintMiddle(int n)
+        {
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write("-");
+
+                for (int j = 0; j < n - 1; j++)
+                {
+                    Console.Write("\\/");
+                }
+
+                Console.Write("-");
+                Console.WriteLine();
+            }
+        }
+
+        private static void PrintDashes(int n)
+        {
+            /*Another way
+             Console.WriteLine(new string("-", 2 * n));
+             */
+            for (int i = 1; i <= 2 * n; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ MethodsDebuggingAndTroubleshooting/PrintingTriangle/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace MethodsDebuggingAndTroubleshooting
+{
+    using System;
+
+    // From SoftUni Programming Fundamentals January 2018 - Lab: Methods, Debugging and Troubleshooting Code
+    public class Program
+    {
+
+        /*Printing a triangle simillar to this one, where the biggest number is taken from the input
+          1 2
+          1 2 3
+          1 2 3 4
+          1 2 3
+          1 2
+          */
+
+        public static void Main()
+        {
+            Console.Write("Number of columns: ");
+            int n = int.Parse(Console.ReadLine());
+
+            PrintTriangle(n);
+        }
+
+        private static void PrintTriangle(int n)
+        {
+            PrintTopPart(n);
+            PrintBottomPart(n);
+        }
+
+        private static void PrintTopPart(int n)
+        {
+            for (int row = 1; row <= n; row++)
+            {
+                PrintLine(row);
+            }
+        }
+
+        private static void PrintBottomPart(int n)
+        {
+            for (int row = n - 1; row >= 1; row--)
+            {
+                PrintLine(row);
+            }
+        }
+
+        private static void PrintLine(int row)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                Console.Write(col + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
